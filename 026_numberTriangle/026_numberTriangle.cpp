464ad3f0e75c172d4fbd404d4e5c6eb86b5c9a38
jeonseobin <seobin3812@naver.com>
2024-10-10 10:34:09 +0900

    Add files via upload

---
+++ 026_numberTriangle/026_numberTriangle.cpp
@@ -0,0 +1,40 @@
+﻿// 026_numberTriangle.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    //int n;
+
+    //printf("n 입력  : ");
+    //scanf_s("%d", &n);
+
+    //for (int i = n; i > 0; i--) {
+    //    for (int j = i; j > 0; j--)
+    //        printf("%d ", j);
+    //    printf("\n");
+    //}
+
+    int n[10];
+    int max, min;
+
+    printf("10개의 숫자 입력 : ");
+    for (int i = 0; i < 10; i++) {
+        scanf_s("%d", &n[i]);
+        if (i == 0) {
+            max = min = n[i];
+        }
+        else {
+            if (n[i] > max) {
+                max = n[i];
+            }
+            if (n[i] < min) {
+                min = n[i];
+            }
+             
+        }
+    }
+}
+
+
---
+++ 027_pyramid/027_pyramid.cpp
@@ -0,0 +1,69 @@
+﻿// 027_pyramid.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("자연수 n 입력 : ");
+    scanf_s("%d", &n);
+
+    printf("\n(1)\n");
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    printf("\n(2)\n");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\n(3)\n");
+    for (int i = 5; i > 0; i--) {
+        for (int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\n(4)\n");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < n - i; j++)
+            printf(" ");
+
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+
+        
+    }
+
+    printf("\n(5)\n");
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < n - i; j++)
+            printf(" ");
+        for (int j = 0; j < 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    printf("\n(6)\n");
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++)
+            printf(" ");
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+}
+
