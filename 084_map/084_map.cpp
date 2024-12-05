f07f6d752b88a14644937309da20883f2a82e1e3
jeonseobin <seobin3812@naver.com>
2024-12-05 10:41:18 +0900

    Add files via upload

---
+++ 084_map/084_map.cpp
@@ -0,0 +1,20 @@
+﻿// 084_map.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	map<string, string> f;
+
+	f["강병익"] = "000-1111-2222";
+	f["조용석"] = "000-3333-4444";
+	f["김병수"] = "000-5555-6666";
+
+	for (auto i : f)
+		cout << i.first << " : " << i.second << endl;
+
+}
+
