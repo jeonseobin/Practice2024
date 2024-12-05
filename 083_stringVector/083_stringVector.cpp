1f02c87f4c6ebaa6f6082624d769452cd9c5eb6a
jeonseobin <seobin3812@naver.com>
2024-12-05 10:32:42 +0900

    Add files via upload

---
+++ 083_stringVector/083_stringVector.cpp
@@ -0,0 +1,26 @@
+﻿// 083_stringVector.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    vector<string> v;
+    string s;
+
+    cout << "문자열 10개 입력 : ";
+    for (int i = 0; i < 10; i++) {
+        cin >> s;
+        v.push_back(s);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < 10; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
